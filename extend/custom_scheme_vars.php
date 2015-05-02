a0e5f4a5a4f17da1d1cb28b1f28b99964a4fb1db
wormeyman <wormeyman@gmail.com>
2015-05-02 15:15:10 -0700

    Initial Commit

---
+++ extend/custom_scheme_vars.php
@@ -0,0 +1,10 @@
+<?php
+// Insert your custom vars here and back up this file before a theme update,
+// so you can override color scheme variables and not lose your changes after theme update
+
+// Located at /inc/colors/default.php
+
+// e.g
+// $scheme_vars[ 'bodyBackgroundColor' ]         = '#f0f3f5';
+$scheme_vars[ 'sidebarWidth' ]                    = '360px';
+?>
