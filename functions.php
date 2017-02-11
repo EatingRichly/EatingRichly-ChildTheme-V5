284a7cd4b29810d76bdf646a0d76ff9c89d19b9c
Eric J <wormeyman@gmail.com>
2017-02-11 14:20:49 -0800

    Comment out staging warning not necessary on LW.

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
 
 /*Alert on Staging Site*/
 /*http://stackoverflow.com/questions/6522023/php-if-domain*/
+/*
 $host = $_SERVER['HTTP_HOST'];
 if ($host === 'staging.eatingrichly.flywheelsites.com') {
     function staging_admin_error_notice()
@@ -28,6 +29,7 @@ if ($host === 'staging.eatingrichly.flywheelsites.com') {
     }
     add_action('admin_notices', 'staging_admin_error_notice');
 }
+*/
 /*End Alert on Staging Site*/
 /*Override Parent Functions*/
 /*http://code.tutsplus.com/tutorials/a-guide-to-overriding-parent-theme-functions-in-your-child-theme--cms-22623*/
